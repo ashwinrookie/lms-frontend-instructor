1333e5a7d53d1ea56e9d18b684829772aa5387c7
Praveen Thirumalai <praveen@Praveens-MacBook-Air.local>
2024-09-30 14:52:59 -0400

    ui changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,11 +7,13 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AuthComponent,
     SignupComponent,
     LoginComponent,
+   
   ],
   imports: [
     CommonModule,
