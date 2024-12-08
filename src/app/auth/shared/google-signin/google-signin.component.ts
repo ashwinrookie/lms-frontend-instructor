8275eba7c20a6d565b995050887d60e84620a64c
Arun Varadharajalu <arunvaradharajalu@gmail.com>
2024-12-08 18:47:21 -0500

    Signin instructor using google oauth

--- src/app/auth/shared/google-signin/google-signin.component.ts
+++ src/app/auth/shared/google-signin/google-signin.component.ts
@@ -2,62 +2,65 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 
+
 @Component({
-  selector: 'app-google-signin',
-  templateUrl: './google-signin.component.html',
-  styleUrls: ['./google-signin.component.scss'],
+	selector: 'app-google-signin',
+	templateUrl: './google-signin.component.html',
+	styleUrls: ['./google-signin.component.scss'],
 })
 export class GoogleSigninComponent {
-  googleClientId: string =
-    '64064225630-87jioglg0r5mievqsoppf6m2qa1lnatc.apps.googleusercontent.com';
-  redirectUri: string = 'http://localhost:4200/auth/login'; // Your redirect URI
+	googleClientId: string =
+		'64064225630-87jioglg0r5mievqsoppf6m2qa1lnatc.apps.googleusercontent.com';
+	redirectUri: string = 'http://localhost:4200/auth/login'; // Your redirect URI
 
-  constructor(
-    private route: ActivatedRoute,
-    private _router: Router,
-    private _authService: AuthService
-  ) {}
+	constructor(
+		private route: ActivatedRoute,
+		private _router: Router,
+		private _authService: AuthService
+	) { }
 
-  // ngAfterViewInit(): void {
-  //   this.route.queryParams.subscribe((params) => {
-  //     const authorizationCode = params['code'];
-  //     if (authorizationCode) {
-  //       console.log('Authorization Code:', authorizationCode);
-  //       this.sendAuthCode(authorizationCode, this.redirectUri);
-  //     }
-  //   });
-  // }
+	ngAfterViewInit(): void {
+		this.route.queryParams.subscribe((params) => {
+			const authorizationCode = params['code'];
+			if (authorizationCode) {
+				console.log('Authorization Code:', authorizationCode);
+				this.sendAuthCode(authorizationCode, this.redirectUri);
+			}
+		});
+	}
 
-  signInWithGoogle(): void {
-    const oauthUrl =
-      `https://accounts.google.com/o/oauth2/v2/auth?` +
-      `client_id=${this.googleClientId}` +
-      `&redirect_uri=${this.redirectUri}` +
-      `&response_type=code` +
-      `&scope=openid%20profile%20email` +
-      `&access_type=offline`;
+	signInWithGoogle(): void {
+		const oauthUrl =
+			`https://accounts.google.com/o/oauth2/v2/auth?` +
+			`client_id=${this.googleClientId}` +
+			`&redirect_uri=${this.redirectUri}` +
+			`&response_type=code` +
+			`&scope=openid%20profile%20email` +
+			`&access_type=offline`;
 
-    // Redirect to Google OAuth 2.0 endpoint
-    window.location.href = oauthUrl;
-  }
+		window.location.href = oauthUrl;
+	}
 
-  // sendAuthCode(authCode: String, redirect_uri: String) {
-  //   const formData = {
-  //     authCode: authCode,
-  //     redirectUri: redirect_uri,
-  //   };
-  //   this._authService.googleSignin(formData).subscribe({
-  //     next: (googleSigninResponse) => {
-  //       console.log(googleSigninResponse);
+	sendAuthCode(authCode: String, redirect_uri: String) {
+		const formData = {
+			authCode: authCode,
+			redirectUri: redirect_uri,
+		};
 
-  //       localStorage.setItem('authToken', googleSigninResponse.accessToken);
-  //       localStorage.setItem('refreshToken', googleSigninResponse.refreshToken);
+		this._authService.googleSignin(formData).subscribe({
+			next: async (googleSigninResponse) => {
+				console.log(googleSigninResponse);
 
-  //       this._router.navigate(['/']);
-  //     },
-  //     error: (error: Error) => {
-  //       console.log(error);
-  //     },
-  //   });
-  // }
+				localStorage.setItem('authToken', googleSigninResponse.accessToken);
+				localStorage.setItem('refreshToken', googleSigninResponse.refreshToken);
+
+				await new Promise(resolve => setTimeout(resolve, 5000));
+
+				this._router.navigate(['/']);
+			},
+			error: (error: Error) => {
+				console.log(error);
+			},
+		});
+	}
 }
